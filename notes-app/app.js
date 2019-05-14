fde02dc2baea759e31e0929edadc6b90503119c3
DavidLapadula <lapadula.david@gmail.com>
2019-05-13 22:02:51 -0400

    [feature] command line functionality added

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,8 +1,12 @@
- const getNotes = require('./notes'); 
+const getNotes = require('./notes'); 
 const validator = require('validator');
+const chalk = require('chalk'); 
+
 
 // console.log(validator.isEmail('example.com')); 
-console.log(validator.isURL('example.com')); 
+//console.log(validator.isURL('example.com')); 
 
+// Print color to the console using chalk
 
+console.log(chalk.green('Success')); 
  
