d1325d1d0bf23143059b9fce39169f98d858aa2e
DavidLapadula <lapadula.david@gmail.com>
2019-05-13 22:09:32 -0400

    [feature] method chaining with chalk

--- notes-app/app.js
+++ notes-app/app.js
@@ -7,6 +7,19 @@ const chalk = require('chalk');
 //console.log(validator.isURL('example.com')); 
 
 // Print color to the console using chalk
+const greenMssg = chalk
+                    .green
+                    .bold
+                    .inverse
+                    ('Success'); 
 
-console.log(chalk.green('Success')); 
+const redMssg = chalk
+                .red
+                .bold
+                .inverse
+                ('Fail'); 
+
+
+console.log(greenMssg); 
+console.log(redMssg); 
  
