53201a4280b9564f1e8002daabae87a2103d1ad3
DavidLapadula <lapadula.david@gmail.com>
2019-05-22 22:04:13 -0400

    [addition] integrated weather and map API's

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,7 +2,18 @@ const request = require('request');
 
 const url = 'https://api.darksky.net/forecast/634ec3f82d1865218236976421b59ef2/37.8267,-122.4233';
 
-request({ url: url }, (error, response) => {
-    const data = JSON.parse(response.body); 
-    console.log(data.currently);
+request({ url: url, json: true }, (error, response) => {
+    console.log(response.body.daily.data[0].summary)
+    console.log(`Its is currently ${response.body.currently.temperature} degrees out.`)  
+    console.log(`There is a ${response.body.currently.precipProbability} chance of rain.`);
+}); 
+
+const geocodeURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiZGF2aWRsYXBhZHVsYSIsImEiOiJjancwMDJ3ZW4wNjA0M3ltdzhrNXlwNjM5In0.C2aiuF07gIUq_PsI15DxnA';
+
+request({url: geocodeURL, json: true}, (error, response) => {
+    const latitude = response.body.features[0].center[1]; 
+    const longitude = response.body.features[0].center[0];
+    
+    console.log(latitude);
+    console.log(longitude);
 }); 
