4b3fab822bd96a7ae6870a258c304255d3229ff1
DavidLapadula <lapadula.david@gmail.com>
2019-05-23 22:20:20 -0400

    [practice] refreshing callbacks with async functions

---
+++ playground/4-callbacks.js
@@ -0,0 +1,32 @@
+setTimeout(() => {
+    console.log('Two Seconds up');
+}, 2000);
+
+const names = ['David', 'Jennifer'];
+const shortNames = names.filter((name) => {
+    return name.length >= 5;
+});
+
+const geocode = (address, callback) => {
+    setTimeout(() => {
+        const data = {
+            latitude: 0,
+            longitude: 0
+        };
+        callback(data) 
+    }, 2000);
+}; 
+
+geocode('Philadephia', (data) => {
+    console.log(data);
+}); 
+
+const add = (a, b, callback) => {
+    setTimeout(() => {
+        callback(a + b); 
+    }, 2000); 
+}; 
+
+add(1, 4, (sum) => {
+    console.log(sum);
+}); 
