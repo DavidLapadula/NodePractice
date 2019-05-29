61ee0a2badaabf5a80480e625d0906c40099a65c
DavidLapadula <lapadula.david@gmail.com>
2019-05-28 22:24:05 -0400

    [addition] added dynamic views with hbs

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,7 +3,29 @@ const express = require('express');
 
 const app = express(); 
 
+app.set('view engine', 'hbs'); 
+
 app.use(express.static(path.join(__dirname, '../public')))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather', 
+        name: 'David'
+    }); 
+}); 
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'David' 
+    }); 
+}); 
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        helptext: 'Helpful text'
+    }); 
+}); 
 
 app.get('/weather', (req, res) => {
     res.send({
