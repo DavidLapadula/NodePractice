8d63c9ddcc9c2e73d92484048064eccdd48ea6e4
DavidLapadula <lapadula.david@gmail.com>
2019-06-12 22:03:46 -0400

    [add] fetch weather data from local api

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,13 @@
 console.log('Client side js here');
+
+fetch('http://localhost:3000/weather?address=boston')
+    .then((response) => {
+        response.json().then((data) => {
+            if(data.error) {
+                console.log(data.error);
+            } else {
+                console.log(data);
+                console.log(location);
+            }; 
+        }); 
+    }); 
--- web-server/src/app.js
+++ web-server/src/app.js
@@ -25,7 +25,7 @@ app.get('', (req, res) => {
         title: 'Weather',
         name: 'David'
     });
-});
+}); 
 
 app.get('/about', (req, res) => {
     res.render('about', {
