4bc99eada590466d172c1b1d2aa6fc72b43fb784
DavidLapadula <lapadula.david@gmail.com>
2019-06-15 16:56:13 -0400

    [addition] instered test data into the DB

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -9,10 +9,54 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
         return console.log('Cant connect to databse');
     }
 
-    const db = client.db(databaseName); 
+    const db = client.db(databaseName);
 
-    db.collection('users').insertOne({
-        name: 'David', 
-        age: 25
+    // db.collection('users').insertOne({
+    //     name: 'David',
+    //     age: 25
+    // }, (error, result) => {
+    //     if(error) {
+    //         return console.log('Cannot inster user');
+    //     }
+
+    //     console.log(result.ops);
+    // });
+
+    // db.collection('users').insertMany([
+    //     {
+    //         name: 'Matthew', 
+    //         age: 25
+    //     }, 
+    //     {
+    //         name: 'John', 
+    //         age: 250
+    //     }
+    // ], (error, result) => {
+    //             if(error) {
+    //         return console.log('Cannot inster documents');
+    //     }
+
+    //     console.log(result.ops);
+    // }); 
+
+    db.collection('tasks').insertMany([
+        {
+            description: 'Shopping',
+            completed: false 
+        },  
+        {
+            description: 'eat',
+            completed: true 
+        },  
+        {
+            description: 'get gas',
+            completed: false 
+        },  
+    ], (error, result) => {
+        if (error) {
+            return console.log('Could not insert tasks')
+        }
+
+        console.log(result.ops);
     }); 
 }); 
