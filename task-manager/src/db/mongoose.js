a295d4aec633932704795930e2fc90d09fabea5e
DavidLapadula <lapadula.david@gmail.com>
2019-06-17 22:17:40 -0400

    [add] data sanitization and validation done with mongoose

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,22 +1,58 @@
 const mongoose = require('mongoose');
+const validator = require('validator'); 
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true
 });
 
-// const User = mongoose.model('User', {
-//     name: {
-//         type: String
-//     },
-//     age: {
-//         type: Number
-//     }
-// });
+// Make New user model
 
+const User = mongoose.model('User', {
+    name: {
+        type: String, 
+        required: true, 
+        trim: true
+    },
+    password:  {
+        type: String, 
+        required: true, 
+        trim: true, 
+        minlength: 6, 
+        validate(value) {
+            if(value.toLowerCase().includes('Password')) {
+                throw new Error('That is not a safe password')
+            }
+        }
+    },  
+    age: {
+        type: Number, 
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age mnust be a positive number')
+            }; 
+        }, 
+        default: 0
+    }, 
+    email: {
+        type: String, 
+        required: true, 
+        trim: true, 
+        lowercase: true, 
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error ('That is not a valid email'); 
+            }; 
+        }
+    }, 
+}); 
+
+// Make a new user and dave it
 // const me = new User({
-//     name: 'David',
-//     age: 26
+//     name: 'John',
+//     password: 'uwishtooshort', 
+//     age: 26, 
+//     email: 'john123@hotmail.com'
 // });
 
 // me.save()
@@ -28,17 +64,23 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
 //     });
 
 
+// New Task and save it
+
 const Task = mongoose.model('Tasks', {
     description: {
-        type: String
+        type: String, 
+        required: true, 
+        trim: true
     }, 
     completed: {
-        type: Boolean
+        type: Boolean, 
+        required: false, 
+        default: false
     }
 }); 
 
 const task = new Task({
-    description: 'Learn', 
+    description: 'Study', 
     completed: false
 })
 
