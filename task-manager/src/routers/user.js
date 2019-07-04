c703cc7df206f6e69c6efdb8cf172d5d9a57257d
DavidLapadula <lapadula.david@gmail.com>
2019-07-03 21:48:00 -0400

    [addition] added support for file uploads with multer

--- task-manager/src/routers/user.js
+++ task-manager/src/routers/user.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = new express.Router();
 const User = require('../models/user');
 const auth = require('../middleware/auth');
+const multer = require('multer'); 
 
 router.post('/users', async (req, res) => {
     const user = new User(req.body);
@@ -77,5 +78,15 @@ router.delete('/users/me', auth, async (req, res) => {
         res.status(500).send(e);
     }
 });
+
+const upload = multer({
+    desc: 'avatars'
+});
+
+router.post('/users/me/avatar', upload.single('avatar'), (req, res) => {
+
+}); 
+
+
 
 module.exports = router; 
