a0c695216986a28b5727fbeb5bc2fc8d53b8ee55
DavidLapadula <lapadula.david@gmail.com>
2019-07-13 11:00:42 -0400

    [addition] added styling format to chat page

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -8,14 +8,23 @@ const $sendLocationButton = document.querySelector('#send-location');
 const $messages = document.querySelector('#messages');
 
 //Templates
-const messageTemplate = document.querySelector('#message-template').innerHTML; 
+const messageTemplate = document.querySelector('#message-template').innerHTML;
+const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML;
+
+socket.on('locationMessage', (message) => {
+    const html = Mustache.render(locationMessageTemplate, {
+        url : message.url, 
+        createdAt: moment(message.createdAt).format('h:mm a')
+    });
+    $messages.insertAdjacentHTML('beforeend', html);
+});
 
 socket.on('message', (message) => {
-    console.log(message);
     const html = Mustache.render(messageTemplate, {
-        message
-    }); 
-    $messages.insertAdjacentHTML('beforeend', html); 
+        message: message.text, 
+        createdAt: moment(message.createdAt).format('h:mm a')
+    });
+    $messages.insertAdjacentHTML('beforeend', html);
 });
 
 $messageForm
